34ace803a3b0e31a9e0cca7a189a58a30db0ed9f
GabrielAldiney <102868870+GabrielAldiney@users.noreply.github.com>
2023-10-22 12:47:09 -0300

    Update Contador.java

--- src/Contador.java
+++ src/Contador.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
         System.out.println("Digite o primeiro parâmetro");
@@ -8,8 +8,10 @@ public class Main {
         System.out.println("Digite o segundo parâmetro");
         int parametroDois = terminal.nextInt();
 
+
         try {
             contar(parametroUm, parametroDois);
+
         } catch (ParametrosInvalidosException e) {
             System.out.println("O segundo parâmetro deve ser maior que o primeiro");
         }
@@ -19,8 +21,8 @@ public class Main {
         if (parametroUm >= parametroDois) {
             throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
         }
-
-        for (int i = parametroUm; i < parametroDois; i++) {
+        int resultado = parametroDois - parametroUm;
+        for (int i = 1; i <= resultado; i++) {
             System.out.println("Imprimindo o número " + i);
         }
     }
